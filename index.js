e2413fcfac9389b32c371335a061886fb647d122
Jbeltrez <beltrezjohn@gmail.com>
2021-07-04 12:54:57 -0400

    commit tree a463bc44a3097896d33036527fc2e375366eafdd

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
+  if (value <= 400) {
+    return "This one is on me!"
+  } else if (value > 2000 && value < 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return (city === 'NYC' ? "Ok, sounds good." : "No go.");
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+
+  switch(tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
+      return "Thank you.";
+    default: 
+      return "Bye."
+  }
+  
   // Write your code here!
 }
